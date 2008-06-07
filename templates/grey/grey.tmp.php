add37b5a9a44c4d25fccb8b95a3dd67bf70b9108
brynk <brynk@hekjeurk.nl>
2008-06-07 00:04:24 +0000

    added cookies for login

--- templates/grey/grey.tmp.php
+++ templates/grey/grey.tmp.php
@@ -4,6 +4,7 @@ A.I.R.C.O
 template
 @author #urk team
 */
+
 echo <<< TEMPLATE
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -21,11 +22,12 @@ echo <<< TEMPLATE
 		<a href="index.php?mod=quotes" title="quotes"><img src="images/quotes.png" width="30" height="30" alt="quotes" /></a>
 		<a href="index.php?mod=pics" title="pics"><img src="images/pics.png" width="30" height="30" alt="pics" /></a>
 		{$extraButtons}
+		<a href="index.php?mod=logout" title="logout"><img src="images/logout.png" width="30" height="30" alt="logout" /></a>
 		#channel {$headerText}
 	</div>
 	<div class="nav">
 		<form method="post">
-			<input type="text" name="search" value="zoeken" onclick="this.value=''" />
+			<input type="text" name="search" value="zoeken" onfocus="this.value=''" />
 			<input class="search" type="image" src="images/search.png" />
 		</form>
 		{$crumbs}
